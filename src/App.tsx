40cd0a45345d0da976e9213a2c22067a8972232b
SignJ <sign.ac@odds.team>
2023-07-13 17:05:14 +0700

    Add Homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 function App() {
-  const name = 'SunnyJ';
+  const name = 'SunnyJ🌤️🎄';
   const githubUrl = 'https://github.com/AnchanaCamchern/Profiles';
   const discordUsername = '@Signjjyd';
-  const imageUrl = `https://cdn.discordapp.com/avatars/397430170484604928/a_98cb5088d11564bec96a4f9c99eb1326.gif`;
+  const imageUrl = `https://i.pinimg.com/originals/01/9a/ed/019aed22e90a64b3fdbb0f1bec78441c.png`;
   const email = `sign.ac@odds.team`;
   const phone = `+(66) 585-XXXX`;
   return (
     <section className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-10 w-full">
-      <div className="flex flex-col md:flex-row items-center justify-start w-full bg-white p-10 rounded-xl">
-        <img
-          src={imageUrl}
-          alt="Profile"
-          className="w-48 h-48 object-cover rounded-full border-4 border-blue-200 mb-10 md:mb-0 md:mr-10"
-        />
+      <div className="flex flex-col md:flex-row items-center justify-center w-full bg-white p-10 rounded-xl">
+        <div>
+          <img
+            src={imageUrl}
+            alt="Profile"
+            className="w-1 h-2 object-cover rounded-full border-4 border-blue-200 mb-10 md:mb-0 md:mr-10"
+          /></div>
+
         <div>
           <h2 className="text-3xl font-bold text-gray-700 mb-4">{`My name is ${name}.`}</h2>
           <h5 className="text-gray-500 mb-2 flex">Discord: <p className="text-blue-500 ml-2 mb-4">{discordUsername}</p></h5>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-0.1/',
+  base: '/Profiles/',
   plugins: [react()],
   build: {
     rollupOptions: {
